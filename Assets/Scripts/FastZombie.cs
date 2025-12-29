138fcf18cca02cac27ffaf666a48ccb2aae68cea
HuntleyAC <huntleygod@icloud.com>
2025-12-29 15:16:08 +0500

    end

--- Assets/Scripts/FastZombie.cs
+++ Assets/Scripts/FastZombie.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class FastZombie : Enemy
 {
     // Быстрый, но хрупкий зомби
-    public float moveSpeed = 4f;
+    public float moveSpeed = 2.6667f;
     [Header("Visual")]
     [SerializeField] private string moveSpriteResourceFolder = "zombie_soldier";
     [SerializeField] private float moveAnimFrameTime = 0.06f;
